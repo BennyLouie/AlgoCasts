af80e5f6ed5735dba074a4b124c016e04098889b
Benny Louie <50751058+BennyLouie@users.noreply.github.com>
2020-02-23 15:09:26 -0500

    added my solutions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,23 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    // 1) return str.split('').reverse().join('')
+
+    // 2) let reversed = []
+    // let split = str.split('')
+    // for (let i = 0; i < str.length; i++) {
+    //     reversed.push(split.pop())
+    // }
+    // return reversed.join('')
+
+    // 3) let reversed = ''
+    // for (let char of str) {
+    //     reversed = char + reversed
+    // }
+    // return reversed
+
+    return str.split('').reduce((reversed, char) => char + reversed, '')
+}
 
 module.exports = reverse;
