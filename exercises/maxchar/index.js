37bcc19d6bd861d87b0742bd82ed5e8a1faa088f
BennyLouie <bennylouie1412@gmail.com>
2020-02-24 16:16:11 -0500

    added my solutions for maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let hash = {}
+    for (let char of str) {
+        hash[char] ? hash[char]++ : hash[char] = 1
+        // hash[char] = hash[char] + 1 || 1 --> If the first is falsey, the value is 1
+    }
+    // 1) let max = Math.max(...Object.values(hash))
+    // return Object.keys(hash).find(key => hash[key] === max)
+
+    // 2)
+    let max = 0
+    let maxChar = ''
+    for (let char in hash) {
+        if (hash[char] > max) {
+            max = hash[char]
+            maxChar = char
+        }
+    }
+    return maxChar
+}
 
 module.exports = maxChar;
